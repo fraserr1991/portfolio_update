3ade08c49fe1cb223f98d83fd907609c16730b3e
Fraser <fraser.russell1991@gmail.com>
2023-01-29 17:56:49 +0000

    The website now grabs details from an API and brings back projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Projects.css'
 import styled from 'styled-components'
 import {Fade} from 'react-reveal'
+import { projectsList } from './ProjectsList'
 
 const ProjectsBox = styled.div`
     height: 40em;
+    display: flex;
+    justify-content: space-evenly;
 `
 
 const Title = styled.h1`
@@ -12,19 +15,94 @@ const Title = styled.h1`
     text-align: center;
 `
 
+const ProjectDetails = styled.div`
+    background-color: whitesmoke;
+    height: 12em;
+    padding: 1em;
+    border-radius: 1em;
+`
+
 const Projects = () => {
+const [repos, setRepos] = useState([])
+
+useEffect(() => {
+    async function getRepos() {
+    const repos = await fetch(
+        'https://api.github.com/users/fraserr1991/repos?per_page=100'
+    )
+    return repos.json()
+    }
+
+    getRepos()
+    .then((data) => {
+        setRepos(data)
+    })
+    .catch((err) => console.error(err))
+}, [])
+
     return(
-    <ProjectsBox id="Projects">
+    <>
     <Fade left>
     <Title>Projects: </Title>
     </Fade>
     <Fade right> 
-        <>
-        <i class="devicon-javascript-plain colored"></i>
-        <i class="devicon-python-plain-wordmark colored"></i>   
-        </>
-    </Fade>
+    <ProjectsBox>
+    {projectsList.map((project) => {
+            return (
+            <ProjectDetails key={project.slug}>
+                <div className="stars">
+                {repos.find((repo) => repo.name === project.slug) && (
+                    <div className="star">
+                    <a
+                        href={`https://github.com/fraserr1991/${project.slug}/stargazers`}
+                    >
+                    </a>
+                    </div>
+                )}
+                </div>
+                <div>
+                <time>{project.date}</time><br/>
+                <a
+                    className="card-header"
+                    href={`https://github.com/fraserr1991/${project.slug}`}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {project.name}
+                </a>
+                <p>{project.tagline}</p>
+                </div>
+                <div className="links anchored">
+                {project.writeup && (
+                    <li className="button small" to={project.writeup}>
+                    Article
+                    </li>
+                )}
+                {project.url && (
+                    <a
+                    className="button small flex"
+                    href={project.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    >
+                    </a>
+                )}
+                <a
+                    className="button small flex"
+                    href={`https://github.com/fraserr1991/${project.slug}`}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                Source
+                </a>
+                <p>Languages: {project.languages}</p>
+                </div>
+            </ProjectDetails>
+            )
+        })}
     </ProjectsBox>
+    </Fade>
+    </>
     )
 }
 
---
+++ src/components/ProjectsList.js
@@ -0,0 +1,16 @@
+export const projectsList = [
+    {
+      name: 'Coffee equipment shop',
+      date: '2022',
+      slug: 'Week_05_Project',
+      tagline: 'A coffee equipment shop',
+      languages: 'HTML, CSS, PostGreSql, Jinja2'
+    },
+    {
+        name: 'Portfolio site',
+        date: '2022',
+        slug: 'portfolio',
+        tagline: 'This webstie, for hosting my own personal portfolio',
+        languages: 'HTML, CSS, React'
+    }
+  ]
